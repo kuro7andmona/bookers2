af7359f42ffe5526fc932ab49a1423098580b3c4
kuro7andmona <129634798+kuro7andmona@users.noreply.github.com>
2023-04-29 06:30:40 +0000

    each変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,6 @@ def create
  @book = Book.new(book_params)
  @book.user_id = current_user.id
  if @book.save
-   flash[:notice] = "You have created book successfully."
    redirect_to book_path(@book.id)
  else
    @user = current_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,8 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
-    #unless user.id == current_user.id
-     # redirect_to users_path
+    unless user.id == current_user.id
+     redirect_to users_path
   end
 
   def update
